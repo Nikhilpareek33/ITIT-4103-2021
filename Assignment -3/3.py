b1dfdf8affebd887daa34dc8df05d810f9055e16
Nikhil Pareek <43985744+Nikhilpareek33@users.noreply.github.com>
2021-10-02 22:27:01 +0530

    Update 3.py

--- Assignment -3/3.py
+++ Assignment -3/3.py
@@ -1,87 +1,45 @@
-
 import numpy as np
-from matplotlib import pyplot as plt
-
-#####################################
-############ DIRECT METHOD ##########
-#####################################
-print("Direct Method")
-
-
-def getPoweredX(deg, X_t):
-    X_pow = X_t.copy()
-    for i in range(2, deg + 1):
-        powmat = (X_t[:, 1] ** i).reshape(X_pow.shape[0], 1)
-        X_pow = np.append(X_pow, powmat, axis=1)
-    return X_pow
-
-
-def calculateEmpricalRisk(yh):
-    error = sum((Y - yh) ** 2)
-    return error / len(Y)
+import pandas as pd
+import math
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import art3d
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import KFold
 
 input=np.random.random((50,1))
 noiseless=np.sin(1+np.square(input))
 noise=np.random.normal(0,0.032,size=(50,1))
 noisefull=noiseless+noise
 
-X = input
-X_train = np.append(np.ones((X.shape[0], 1)), X, axis=1)
-Y = noiseless
+Ytrain=noisefull[:40]
+Ytest=noisefull[40:]
+Xtrain=input[:40]
+Xtest=input[40:]
 
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+b = Ytrain.reshape(Ytrain.shape[0],1)
+A = np.concatenate((Xtrain, np.ones((Xtrain.shape[0], 1))), axis=1)
+z = np.dot(np.dot(inv(np.dot(A.T,A)),A.T),b)
 
-# For degree 1 polynomial
-deg1 = 1
-X1 = getPoweredX(deg1, X_train)
+y1 = z[0] * Xtrain + z[1]
+y2 = z[0] * Xtest + z[1]
 
-first_part = np.linalg.inv(np.dot(X1.transpose(), X1))
-second_part = np.dot(first_part, X1.transpose())
-W1 = np.dot(second_part, Y)
+plt.subplot(1,2,1)
+plt.title('Training set')
+plt.scatter(Xtrain, y1,label='model output',c='r')
+plt.scatter(Xtrain, Ytrain,label='actual output',c='g')
+plt.legend()
 
-ax1.scatter(X, Y)
-ax1.scatter(X1[:,1], np.dot(X1, W1))
-ax1.set_title("Degree=1")
-print("Empirical risk for degree 1 model: ", calculateEmpricalRisk(np.dot(X1, W1)))
+plt.subplot(1,2,2)
+plt.title('Testing set')
+plt.scatter(Xtest, y2,label='model output',c='r')
+plt.scatter(Xtest, Ytest,label='actual output',c='g')
+plt.legend()
 
-# For degree 2 polynomial
-deg2 = 2
-X2 = getPoweredX(deg2, X_train)
+MSE = np.square(np.subtract(Ytrain, y1)).mean()
+RMSE = math.sqrt(MSE)
+MSE1 = np.square(np.subtract(Ytest, y2)).mean()
+RMSE1 = math.sqrt(MSE1)
+print('train rmse - ',RMSE,'test rmse -',RMSE)
 
-first_part = np.linalg.pinv(np.dot(X2.transpose(), X2))
-second_part = np.dot(first_part, X2.transpose())
-W2 = np.dot(second_part, Y)
-
-ax2.scatter(X, Y)
-ax2.scatter(X2[:,1], np.dot(X2, W2))
-ax2.set_title("Degree=2")
-print("Empirical risk for degree 2 model: ", calculateEmpricalRisk(np.dot(X2, W2)))
-
-# For degree 3 polynomial
-deg3 = 3
-X3 = getPoweredX(deg3, X_train)
-
-first_part = np.linalg.pinv(np.dot(X3.transpose(), X3))
-second_part = np.dot(first_part, X3.transpose())
-W3 = np.dot(second_part, Y)
-
-ax3.scatter(X, Y)
-ax3.scatter(X3[:,1], np.dot(X3, W3))
-ax3.set_title("Degree=3")
-print("Empirical risk for degree 3 model: ", calculateEmpricalRisk(np.dot(X3, W3)))
-
-# For degree 4 polynomial
-deg4 = 4
-X4 = getPoweredX(deg4, X_train)
-
-first_part = np.linalg.pinv(np.dot(X4.transpose(), X4))
-second_part = np.dot(first_part, X4.transpose())
-W4 = np.dot(second_part, Y)
-
-
-ax4.scatter(X, Y)
-
-ax4.scatter(X4[:,1],np.dot(X4,W4))
-ax4.set_title("Degree=4")
-print("Empirical risk for degree 4 model: ", calculateEmpricalRisk(np.dot(X4, W4)))
 plt.show()
